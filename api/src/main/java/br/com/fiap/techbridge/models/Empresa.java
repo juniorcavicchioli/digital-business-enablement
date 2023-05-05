93f6c16255c81766e29818c03ada51b1262158db
juniorcavicchioli <cavicchioli.adilson@gmail.com>
2023-05-04 23:16:24 -0300

    HATEOAS Empresa

--- api/src/main/java/br/com/fiap/techbridge/models/Empresa.java
+++ api/src/main/java/br/com/fiap/techbridge/models/Empresa.java
@@ -1,5 +1,7 @@
 package br.com.fiap.techbridge.models;
 
+import br.com.fiap.techbridge.controllers.AvaliacaoController;
+import br.com.fiap.techbridge.controllers.EmpresaController;
 import br.com.fiap.techbridge.valueobjects.Endereco;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
@@ -7,16 +9,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
 
 import java.util.ArrayList;
 import java.util.List;
+//import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class Empresa{
+public class Empresa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_empresa", nullable = false)
@@ -34,6 +41,16 @@ public class Empresa{
 
     @Embedded
     private Endereco endereco;
+
+    public EntityModel<Empresa> toEntityModel() {
+        return EntityModel.of(
+                this,
+                linkTo(methodOn(EmpresaController.class).show(id)).withSelfRel(),
+                linkTo(methodOn(EmpresaController.class).destroy(id)).withRel("delete"),
+                linkTo(methodOn(EmpresaController.class).index(null, Pageable.unpaged())).withRel("all"),
+                linkTo(methodOn(AvaliacaoController.class).index(String.valueOf(id), null, Pageable.unpaged())).withRel("avaliacoes")
+        );
+    }
 
 //    @OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL)
 //    private List<Avaliacao> avaliacoes = new ArrayList<>();
